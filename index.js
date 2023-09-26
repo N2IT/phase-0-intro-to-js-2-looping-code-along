120e4fdc1656a1b61041de42285704d64e77d9c0
Tony Eder <tonyeder11@gmail.com>
2023-09-26 11:11:41 -0700

    Passed the 3 tests

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solutions in this file
+//create a new empty array to hold the messages;
+let cardsMessage = []
+
+//context of card message
+const cardContent = "surprise";
+
+//names for thank you cards
+// const cardNames = ["Xavi", "Cathy", "Manny"];
+
+//create a custom message for each name inside the loop
+function writeCards(cardNames) {
+    for (let i = 0; i < cardNames.length; i++) {
+        cardsMessage.push(`Thank you, ${cardNames[i]}, for the wonderful ${cardContent} gift!`);
+
+    }
+    return cardsMessage;
+}
+
+writeCards(cardNames);
+
+
+function countDown() {
+let number = 10;
+while (number >= 0) {
+    console.log(number--);
+    debugger;
+    }
+}
+
